b260a8dfbf07c03eae7601c2a94b6a994336b266
Mohabbat <35231072+mohbz-0@users.noreply.github.com>
2022-12-09 11:23:17 +0000

    Main work push

--- primes.py
+++ primes.py
@@ -1,6 +1,30 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+#
+#
+# Abdurrahmaan Mohabbat
+# 09/12/2022
+# List Of Primes Generator
+#
 
-def primes(number_of_primes):
-    list = []
-    return list
+def primes(num):
+    if num < 1:
+        raise ValueError('Input Too Low. Input number greater than 0')
+    primeList = []
+    for c in range(num):
+        if len(primeList) == 0:
+            i = 1
+        else:
+            i = primeList[len(primeList)-1]
+        run = True
+        while run:
+            i+=1
+            for d in range(i):
+
+                if d == 0:
+                    pass
+                elif d+1 == i:
+                    primeList.append(i)
+                    run = False
+                    break
+                elif (i % (d+1)) == 0:
+                    break
+    return primeList
